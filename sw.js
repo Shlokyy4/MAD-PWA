2dae2652318eeacb369757e190649b59c7ed933d
Your Name <your-email@example.com>
2025-03-26 12:17:08 +0530

    CHange

--- sw.js
+++ sw.js
@@ -3,10 +3,10 @@ self.addEventListener('install', event => {
   event.waitUntil(
     caches.open('v1').then(cache => {
       return cache.addAll([
-        '/',
-        '/index.html',
-        '/style.css',
-        '/app.js'
+        './',
+        './index.html',
+        './style.css',
+        './app.js'
       ]);
     })
   );
